3d6f4f67ec2d76c6afa2b363a114823cea524859
Raj Sethi <rajs@myidealprop.com>
2017-02-06 16:19:55 -0500

    Removed the test API in LeadsController

--- PropertyWebAPI/Controllers/LeadsController.cs
+++ PropertyWebAPI/Controllers/LeadsController.cs
@@ -16,68 +16,6 @@
     [Authorize]
     public class LeadsController : ApiController
     {
-        /// <summary>  
-        /// Use this api to get a list of leads satisfying the criteria provided. At least one filter must exist. All filters support
-        /// multiple values separated by commas. For string values wildcards are provided. Wildcards is denoted by '*' 
-        /// </summary>  
-        /// <param name="counties">Optional parameter. Multiple values can be sent. No wildcards.</param>
-        /// <param name="buildingclasscodes">Optional parameter. Multiple values can be sent along with wildcards. For example: A*,B1</param>
-        /// <param name="ismailingaddressactive">Optional parameter. Valid Values are Y, N or blank. If you send any other value the filter is ignored</param>
-        /// <param name="isvacant">Optional parameter. Valid Values are Y, N or blank. If you send any other value the filter is ignored</param>
-        /// <param name="zipcodes">Optional parameter. Multiple values can be sent. No wildcards.</param>
-        /// <param name="violations">Optional parameter. Minimum one value, max two values can be sent. No wildcards.</param>
-        /// <param name="lientypes">Optional parameter. Multiple values can be sent. Valid values are MORTGAGE FORECLOSURES, TAX LIENS and ALL. 
-        /// By default if multiple values are provided the filter applies the OR operator between them. Add an additional value OPERATORAND if
-        /// the operator of choice is AND instead of OR. Eg: 1) MORTGAGE FORECLOSURES,TAX LIENS 2) ALL,OPERATORAND</param>
-        /// <param name="states">Optional parameter. Multiple values (state abbreviations) can be sent. No wildcards.</param>
-        /// <param name="cities">Optional parameter. Multiple values can be sent. No wildcards.</param>
-        /// <param name="leadgrades">Optional parameter. Multiple values can be sent. No wildcards</param>
-        /// <param name="ltv">Optional parameter. Minimum one value, max two values can be sent. No wildcards</param>
-        /// <param name="neighborhoods">Optional parameter. Multiple values can be sent along with wildcards.</param>
-        /// <param name="equity">Optional parameter. Minimum one value, max two values can be sent. No wildcards.</param>
-        /// <param name="isfannie">Optional parameter. Valid Values are Y, N. Any other value the filter is ignored</param>
-        /// <param name="isfreddie">Optional parameter. Valid Values are Y, N. Any other value the filter is ignored</param>
-        /// <param name="unbuilt">Optional parameter. Valid Values are Y, N. Any other value the filter is ignored</param>
-        /// <param name="servicer">Optional parameter. Multiple values can be sent along with wildcards.</param>
-        /// <returns>Returns a list of leads (properties)</returns>
-        [Route("api/leads/")]
-        [ResponseType(typeof(List<DAL.LeadSummaryData>))]
-        public IHttpActionResult Get(string zipcodes = null, string neighborhoods = null, string isvacant = null, string leadgrades = null,
-                                     string buildingclasscodes = null, string counties = null, string ismailingaddressactive = null,
-                                     string lientypes = null, string ltv = null, string equity = null, string violations = null,
-                                     string cities = null, string states= null, string isfannie=null, string isfreddie=null, string unbuilt=null, string servicer=null)
-        {
-            if (zipcodes==null && buildingclasscodes==null && counties==null && 
-                isvacant==null && violations==null && ismailingaddressactive == null &&
-                cities==null && neighborhoods==null && states==null && lientypes==null &&
-                leadgrades==null && ltv==null && equity==null && isfannie==null && isfreddie==null &&
-                unbuilt==null && servicer==null)
-                return this.BadRequest("At least one filter is required");
-
-            try
-            {
-                using (GPADBEntities1 gpaE = new GPADBEntities1())
-                {
-                    var leadList = Mapper.Map<List<vwGeneralLeadInfomation>, List<DAL.LeadSummaryData>>(gpaE.GetLeads(zipcodes, buildingclasscodes, counties, isvacant, ismailingaddressactive, violations,
-                                                                                                         cities, neighborhoods, states, lientypes, leadgrades, ltv, equity, isfannie,
-                                                                                                         isfreddie, unbuilt, servicer).ToList());
-                    if (leadList == null || leadList.Count == 0)
-                        return NotFound();
-                    return Ok(leadList);
-                }
-                /*
-                var leadList = DAL.Lead.GetPropertyLeads(zipcodes, buildingclasscodes, counties, isvacant, ismailingaddressactive, violations, cities, neighborhoods, states, 
-                                                         lientypes,leadgrades,ltv,equity,isfannie,isfreddie,unbuilt).ToList();
-                */
-                
-                
-            }
-            catch (Exception e)
-            {
-                Common.Logs.log().Error(string.Format("Exception encountered while retrieving Leads{0}",Common.Utilities.FormatException(e)));
-                return Common.HttpResponse.InternalError(Request, "Internal Error in processing request");
-            }
-        }
 
         /// <summary>  
         /// Use this api to get a lead details
@@ -109,9 +47,33 @@
             }
         }
 
-        [Route("api/leads/test")]
+        /// <summary>  
+        /// Use this api to get a list of leads satisfying the criteria provided. At least one filter must exist. All filters support
+        /// multiple values separated by commas. For string values wildcards are provided. Wildcard is denoted by '*' 
+        /// </summary>  
+        /// <param name="counties">Optional parameter. Multiple values can be sent. No wildcards.</param>
+        /// <param name="buildingclasscodes">Optional parameter. Multiple values can be sent along with wildcards. For example: A*,B1</param>
+        /// <param name="ismailingaddressactive">Optional parameter. Valid Values are Y, N or blank. If you send any other value the filter is ignored</param>
+        /// <param name="isvacant">Optional parameter. Valid Values are Y, N or blank. If you send any other value the filter is ignored</param>
+        /// <param name="zipcodes">Optional parameter. Multiple values can be sent. No wildcards.</param>
+        /// <param name="violations">Optional parameter. Minimum one value, max two values can be sent. No wildcards.</param>
+        /// <param name="lientypes">Optional parameter. Multiple values can be sent. Valid values are MORTGAGE FORECLOSURES, TAX LIENS, TAX LIEN LPS and ALL. 
+        /// By default if multiple values are provided the filter applies the OR operator between them. Add an additional value OPERATORAND if
+        /// the operator of choice is AND instead of OR. Eg: 1) MORTGAGE FORECLOSURES,TAX LIENS 2) ALL,OPERATORAND</param>
+        /// <param name="states">Optional parameter. Multiple values (state abbreviations) can be sent. No wildcards.</param>
+        /// <param name="cities">Optional parameter. Multiple values can be sent. No wildcards.</param>
+        /// <param name="leadgrades">Optional parameter. Multiple values can be sent. No wildcards</param>
+        /// <param name="ltv">Optional parameter. Minimum one value, max two values can be sent. No wildcards</param>
+        /// <param name="neighborhoods">Optional parameter. Multiple values can be sent along with wildcards.</param>
+        /// <param name="equity">Optional parameter. Minimum one value, max two values can be sent. No wildcards.</param>
+        /// <param name="isfannie">Optional parameter. Valid Values are Y, N. Any other value the filter is ignored</param>
+        /// <param name="isfreddie">Optional parameter. Valid Values are Y, N. Any other value the filter is ignored</param>
+        /// <param name="unbuilt">Optional parameter. Valid Values are Y, N. Any other value the filter is ignored</param>
+        /// <param name="servicer">Optional parameter. Multiple values can be sent along with wildcards.</param>
+        /// <returns>Returns a list of leads (properties)</returns>
+        [Route("api/leads/")]
         [ResponseType(typeof(List<DAL.LeadSummaryData>))]
-        public IHttpActionResult GetListTest(string zipcodes = null, string neighborhoods = null, string isvacant = null, string leadgrades = null,
+        public IHttpActionResult GetLeads(string zipcodes = null, string neighborhoods = null, string isvacant = null, string leadgrades = null,
                                      string buildingclasscodes = null, string counties = null, string ismailingaddressactive = null,
                                      string lientypes = null, string ltv = null, string equity = null, string violations = null,
                                      string cities = null, string states = null, string isfannie = null, string isfreddie = null, string unbuilt = null, string servicer=null)
@@ -137,5 +99,47 @@
                 return Common.HttpResponse.InternalError(Request, "Internal Error in processing request");
             }
         }
+
+
+        /*
+        [Route("api/leads/Old")]
+        [ResponseType(typeof(List<DAL.LeadSummaryData>))]
+        public IHttpActionResult GetOld(string zipcodes = null, string neighborhoods = null, string isvacant = null, string leadgrades = null,
+                                     string buildingclasscodes = null, string counties = null, string ismailingaddressactive = null,
+                                     string lientypes = null, string ltv = null, string equity = null, string violations = null,
+                                     string cities = null, string states= null, string isfannie=null, string isfreddie=null, string unbuilt=null, string servicer=null)
+        {
+            if (zipcodes==null && buildingclasscodes==null && counties==null && 
+                isvacant==null && violations==null && ismailingaddressactive == null &&
+                cities==null && neighborhoods==null && states==null && lientypes==null &&
+                leadgrades==null && ltv==null && equity==null && isfannie==null && isfreddie==null &&
+                unbuilt==null && servicer==null)
+                return this.BadRequest("At least one filter is required");
+
+            try
+            {
+                using (GPADBEntities1 gpaE = new GPADBEntities1())
+                {
+                    var leadList = Mapper.Map<List<vwGeneralLeadInfomation>, List<DAL.LeadSummaryData>>(gpaE.GetLeads(zipcodes, buildingclasscodes, counties, isvacant, ismailingaddressactive, violations,
+                                                                                                         cities, neighborhoods, states, lientypes, leadgrades, ltv, equity, isfannie,
+                                                                                                         isfreddie, unbuilt, servicer).ToList());
+                    if (leadList == null || leadList.Count == 0)
+                        return NotFound();
+                    return Ok(leadList);
+                }
+                
+               // var leadList = DAL.Lead.GetPropertyLeads(zipcodes, buildingclasscodes, counties, isvacant, ismailingaddressactive, violations, cities, neighborhoods, states, 
+               //                                          lientypes,leadgrades,ltv,equity,isfannie,isfreddie,unbuilt).ToList();
+                
+                
+                
+            }
+            catch (Exception e)
+            {
+                Common.Logs.log().Error(string.Format("Exception encountered while retrieving Leads{0}",Common.Utilities.FormatException(e)));
+                return Common.HttpResponse.InternalError(Request, "Internal Error in processing request");
+            }
+        }
+*/
     }
 }
